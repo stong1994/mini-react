6615d0c191e76aa52a50c42c835457b408c6ecfb
stong <498327580@qq.com>
2024-03-25 00:25:26 +0800

    extract app.js

--- d1/main.js
+++ d1/main.js
@@ -1,7 +1,5 @@
 import ReactDom from './core/ReactDom.js'
-import React from './core/React.js'
-
-const App = React.createElement("div", { id: "app" }, "hello", "world")
+import App from './App.js'
 
 
 ReactDom.createRoot(document.querySelector("#root")).render(App);

39cf4c761da491218530bf676c3b22e45d47dba1
stong <498327580@qq.com>
2024-03-25 01:41:07 +0800

    jsx

---
+++ d1/vite-project/App.jsx
@@ -0,0 +1,6 @@
+import React from './core/React.js'
+
+// const App = React.createElement("div", { id: "app" }, "hello", "world")
+const App = <div>hello</div>
+
+export default App;
---
+++ d1/vite-project/main.js
@@ -0,0 +1,5 @@
+
+import ReactDom from './core/ReactDom.js'
+import App from './App.jsx'
+
+ReactDom.createRoot(document.querySelector("#root")).render(App);
